21d16d219fa595a44e3fe48c6afa2e587c8c731a
ta.martins <ta.martins@campus.fct.unl.pt>
2020-07-18 23:10:59 +0100

    1st Commit
    Initial Developments

---
+++ Assets/Scenes/Scripts/Player_Controls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controls : MonoBehaviour
+{
+    private Rigidbody rBody;
+    public float playerSpeed = 0.5f;
+    Vector3 startposition;
+    Vector3 position;
+
+
+    void Start()
+    {
+        startposition = transform.position;
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        position = new Vector3(moveH, 0, moveV);
+        rBody.AddForce(position * playerSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cube")
+        {
+            position = startposition;
+        }
+    }
+}
