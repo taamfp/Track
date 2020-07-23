a72491ad00bab5b24013dbef3acfda715ce92d26
ta.martins <ta.martins@campus.fct.unl.pt>
2020-07-23 16:57:02 +0100

    2nd Commit

---
+++ Assets/Assets/Object_Position.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Position : MonoBehaviour
+{
+
+    private Rigidbody rBodyCube;
+    Vector3 startPositionCube;
+
+
+    void Start()
+    {
+        startPositionCube = transform.position;
+        rBodyCube = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        if (transform.position.y < -1.5)
+        {
+            transform.position = startPositionCube;
+        }
+
+    }
+
+        private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            transform.position = startPositionCube;
+        }
+    }
+
+}
---
+++ Assets/Assets/Scenes/Scripts/Camera_Control.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+
+    //Same as player speed
+    public float cameraSpeed = 10.0f;
+
+
+    void Update()
+    {
+        Vector3 local = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            local.z += -cameraSpeed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey("a"))
+        {
+            local.z += cameraSpeed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey("s"))
+        {
+            local.x += -cameraSpeed * Time.deltaTime;
+        }
+
+        else if (Input.GetKey("t"))
+        {
+            local.x += cameraSpeed * Time.deltaTime;
+        }
+
+        transform.position = local;
+    }
+}
